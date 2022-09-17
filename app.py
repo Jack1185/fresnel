3830fc8217f7191c6f42a9cd4f68d184eded8094
Jackson <jackson.fresnel@jfresnel.com>
2022-09-17 23:20:15 +0000

    hi

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 print("Hello World")
 print("This is second commit")
 print("I love you Jesus")
+<<<<<<< HEAD
 print("This is my dev1 branch")
 print("hihi")
+=======
+>>>>>>> parent of d2b59e7 (dev1)
